fa0a6c67d66a561918d2686236626988d67bba2f
Joseph Lander <josephclander@gmail.com>
2021-02-17 11:33:12 +0000

    Refactor: Global vars, fns independent, msg box
    Functions referenced each others vars
    Needed to keep independant purpose.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@
  * Treehouse Techdegree:
  * FSJS Project 2 - Data Pagination and Filtering
  */
+const page = document.querySelector('.page');
+const header = document.querySelector('.header');
+const studentList = document.querySelector('.student-list');
+const linkList = document.querySelector('.link-list');
+const messageBox = document.querySelector('.no-results');
+
+/**
+ * Append a search bar and set listener
+ */
+const searchbar = `<label for="search" class="student-search">
+                     <input id="search" placeholder="Search by name...">
+                     <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+                  </label>`;
+header.insertAdjacentHTML('beforeend', searchbar);
+const searchInput = document.querySelector('#search');
+const searchButton = searchInput.nextElementSibling;
+searchButton.addEventListener('click', () => {
+  search(data);
+});
 
 /**
  * create and append 9 list items to ul in document
@@ -11,8 +30,11 @@
 function showPage(list, page) {
   const startIndex = page * 9 - 9;
   const endIndex = page * 9;
-  const ul = document.querySelector('.student-list');
-  ul.innerHTML = '';
+  // reset list
+  studentList.innerHTML = '';
+  // validation for null lists
+  if (list.length == 0) return false;
+  // Add students to page
   for (let i = 0; i < list.length; i++) {
     const student = list[i];
     // check the student is in the page range
@@ -27,7 +49,7 @@ function showPage(list, page) {
                <span class="date">Joined ${student.registered.date}</span>
             </div>
          </li>`;
-      ul.insertAdjacentHTML('beforeend', text);
+      studentList.insertAdjacentHTML('beforeend', text);
     }
   }
 }
@@ -38,50 +60,34 @@ function showPage(list, page) {
  */
 function addPagination(list) {
   const numberOfPages = Math.ceil(list.length / 9);
-  const ul = document.querySelector('.link-list');
-  ul.innerHTML = '';
+  // reset list
+  linkList.innerHTML = '';
+  // validation for null lists
+  if (list.length == 0) return false;
+  // Add buttons for each page
   for (let i = 0; i < numberOfPages; i++) {
     let li = `<li>
                   <button type="button">${i + 1}</button>
                </li>`;
-    ul.insertAdjacentHTML('beforeend', li);
+    linkList.insertAdjacentHTML('beforeend', li);
   }
   // give active class to first button element
-  ul.firstElementChild.firstElementChild.className = 'active';
-  ul.addEventListener('click', (e) => {
+  linkList.firstElementChild.firstElementChild.className = 'active';
+  linkList.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
       const button = e.target;
       const selectedPage = button.textContent;
-      const listItems = ul.children;
+      const listItems = linkList.children;
       for (let li of listItems) {
-        // select button element
+        // reset button elements by removing class
         li.firstElementChild.className = '';
       }
+      // selected button becomes active
       button.className = 'active';
       showPage(data, selectedPage);
     }
   });
 }
-
-showPage(data, 1);
-addPagination(data);
-
-/**
- * Add a search bar
- */
-const header = document.querySelector('.header');
-const searchbar = `<label for="search" class="student-search">
-                     <input id="search" placeholder="Search by name...">
-                     <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
-                  </label>`;
-header.insertAdjacentHTML('beforeend', searchbar);
-
-// implement search
-const searchInput = document.querySelector('#search');
-const searchButton = searchInput.nextElementSibling;
-searchButton.addEventListener('click', () => {
-  search(data);
-});
 
 /**
  * search function to filter data for matching names
@@ -93,22 +99,16 @@ function search(list) {
     const fullName = `${student.name.first} ${student.name.last}`;
     return fullName.match(searchText);
   });
+  // reset message box
+  messageBox.innerHTML = '';
   // check for no results
-  const page = document.querySelector('.page');
-  if (filtered.length > 0) {
-    // check for previous "no results" text and remove
-    const noResultsElement = document.querySelector('.no-results');
-    if (noResultsElement) page.removeChild(noResultsElement);
-    showPage(filtered, 1);
-    addPagination(filtered);
-  } else {
-    // clear lists
-    const studentList = document.querySelector('.student-list');
-    const linkList = document.querySelector('.link-list');
-    studentList.innerHTML = '';
-    linkList.innerHTML = '';
-    // add "no results" message to page
-    const noResultsText = `<div class="no-results">No results found</div>`;
-    page.insertAdjacentHTML('beforeend', noResultsText);
+  if (filtered.length == 0) {
+    messageBox.textContent = 'No results found';
   }
+  showPage(filtered, 1);
+  addPagination(filtered);
 }
+
+// Initialise the page
+showPage(data, 1);
+addPagination(data);
